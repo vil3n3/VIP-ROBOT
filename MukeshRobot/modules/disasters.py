1d6769bccfd1832f4c78a9192d0f6f1056556413
vil3n3 <138990052+vil3n3@users.noreply.github.com>
2023-08-02 21:36:55 +0530

    Update disasters.py

--- MukeshRobot/modules/disasters.py
+++ MukeshRobot/modules/disasters.py
@@ -630,7 +630,7 @@ __help__ = f"""
 `⚠️ ʀᴇᴀᴅ ғʀᴏᴍ ᴛᴏᴘ`
 ᴠɪsɪᴛ [sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](f"https://t.me{SUPPORT_CHAT}") ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](f"https://t.me/the_vip_boy")............☆
+☆............𝙱𝚈 » [𓆰ꪑ𝓲𝓽𝘴𝓾𝘳𝓲  𝘬ꪖ ꪜ𝓲ꪶꪶ𝓲ꪖꪀ𓆪](f"https://t.me/villen_012")............☆
 """
 
 SUDO_HANDLER = CommandHandler("addsudo", addsudo)
