cc9ed6790c81b011bef1e7a95b8a4930a091e73b
vil3n3 <138990052+vil3n3@users.noreply.github.com>
2023-08-27 14:40:45 +0530

    Update gettime.py

--- MukeshRobot/modules/gettime.py
+++ MukeshRobot/modules/gettime.py
@@ -99,7 +99,7 @@ __help__ = """
 
 💡 ᴇx:- /time ɪɴ *:* ɪᴛ ᴡɪʟʟ sʜᴏᴡs ɪɴᴅɪᴀɴ ᴄᴜʀʀᴇɴᴛ ᴛɪᴍᴇ ᴀɴᴅ ᴅᴀᴛᴇ..
 
-☆............𝙱𝚈 » [𓆰ꪑ𝓲𝓽𝘴𝓾𝘳𝓲  𝘬ꪖ ꪜ𝓲ꪶꪶ𝓲ꪖꪀ𓆪](https://t.me/villen_012)............☆
+☆............𝙱𝚈 » [ꪜ𝓲ꪶꪶ𝓲ꪖꪀ 𝐴𝑠𝑠𝑖𝑠𝑡𝑎𝑛𝑡](https://t.me/villen_012)............☆
 """
 
 TIME_HANDLER = DisableAbleCommandHandler("time", gettime)
