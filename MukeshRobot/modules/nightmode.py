24005d2de39bcc0ff1bc5dd780ceae96b7757318
vil3n3 <138990052+vil3n3@users.noreply.github.com>
2023-08-27 12:07:53 +0530

    Add files via upload

--- MukeshRobot/modules/nightmode.py
+++ MukeshRobot/modules/nightmode.py
@@ -1,164 +1,111 @@
+import random 
+from pyrogram import filters,Client,enums
+from Hiroko import Hiroko
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from telethon import functions, types
-from telethon.tl.types import ChatBannedRights
-from MukeshRobot import BOT_NAME
-from MukeshRobot import telethn as tbot
-from MukeshRobot.events import register
-from MukeshRobot.modules.sql.night_mode_sql import (
-    add_nightmode,
-    get_all_chat_id,
-    is_nightmode_indb,
-    rmnightmode,
-)
+from pyrogram.types import ChatPermissions
+from Hiroko.Helper.database.nightmodedb import nightdb,nightmode_on,nightmode_off,get_nightchats 
 
 
-async def is_register_admin(chat, user):
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
 
-        return isinstance(
-            (
-                await tbot(functions.channels.GetParticipantRequest(chat, user))
-            ).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
-        )
-    elif isinstance(chat, types.InputPeerChat):
-
-        ui = await tbot.get_peer_id(user)
-        ps = (
-            await tbot(functions.messages.GetFullChatRequest(chat.chat_id))
-        ).full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
-        )
-    else:
-        return None
+CLOSE_CHAT = ChatPermissions(
+    can_send_messages=False,
+    can_send_media_messages = False,
+    can_send_other_messages = False,
+    can_send_polls = False,
+    can_change_info = False,
+    can_add_web_page_previews = False,
+    can_pin_messages = False,
+    can_invite_users = False )
 
 
-hehes = ChatBannedRights(
-    until_date=None,
-    send_messages=True,
-    send_media=True,
-    send_stickers=True,
-    send_gifs=True,
-    send_games=True,
-    send_inline=True,
-    send_polls=True,
-    invite_users=True,
-    pin_messages=True,
-    change_info=True,
-)
-openhehe = ChatBannedRights(
-    until_date=None,
-    send_messages=False,
-    send_media=False,
-    send_stickers=False,
-    send_gifs=False,
-    send_games=False,
-    send_inline=False,
-    send_polls=False,
-    invite_users=False,
-    pin_messages=False,
-    change_info=False,
-)
+OPEN_CHAT = ChatPermissions(
+    can_send_messages=True,
+    can_send_media_messages = True,
+    can_send_other_messages = True,
+    can_send_polls = True,
+    can_change_info = True,
+    can_add_web_page_previews = True,
+    can_pin_messages = True,
+    can_invite_users = True )
+    
+buttons = InlineKeyboardMarkup([[InlineKeyboardButton("๏ ᴇɴᴀʙʟᴇ ๏", callback_data="add_night"),InlineKeyboardButton("๏ ᴅɪsᴀʙʟᴇ ๏", callback_data="rm_night")]])         
 
-
-@register(pattern="^/nightmode")
-async def close_ws(event):
-    if event.is_group:
-        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-            await event.reply("🤦🏻‍♂️You are not admin so you can't use this command...")
-            return
-
-    if not event.is_group:
-        await event.reply("You Can Only Enable Night Mode in Groups.")
+@Hiroko.on_message(filters.command("nightmode") & filters.group)
+async def _nightmode(_, message):
+    return await message.reply_photo(photo="https://telegra.ph//file/06649d4d0bbf4285238ee.jpg", caption="**ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ ᴇɴᴀʙʟᴇ ᴏʀ ᴅɪsᴀʙʟᴇ ɴɪɢʜᴛᴍᴏᴅᴇ ɪɴ ᴛʜɪs ᴄʜᴀᴛ.**",reply_markup=buttons)
+              
+     
+@Hiroko.on_callback_query(filters.regex("^(add_night|rm_night)$"))
+async def nightcb(app : Client, query : CallbackQuery):
+    data = query.data 
+    chat_id = query.message.chat.id
+    user_id = query.from_user.id
+    check_night = await nightdb.find_one({"chat_id" : chat_id})
+    administrators = []
+    async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+        administrators.append(m.user.id)     
+    if user_id in administrators:   
+        if data == "add_night":
+            if check_night:        
+                await query.message.edit_caption("**๏ ɴɪɢʜᴛᴍᴏᴅᴇ ɪs ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ ɪɴ ᴛʜɪs ᴄʜᴀᴛ.**")
+            elif not check_night :
+                await nightmode_on(chat_id)
+                await query.message.edit_caption("**๏ ᴀᴅᴅᴇᴅ ᴄʜᴀᴛ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ . ᴛʜɪs ɢʀᴏᴜᴘ ᴡɪʟʟ ʙᴇ ᴄʟᴏsᴇᴅ ᴏɴ 𝟷𝟸ᴀᴍ [IST] ᴀɴᴅ ᴡɪʟʟ ᴏᴘᴇɴᴇᴅ ᴏɴ 𝟶𝟼ᴀᴍ [IST] .**") 
+        if data == "rm_night":
+            if check_night:  
+                await nightmode_off(chat_id)      
+                await query.message.edit_caption("**๏ ɴɪɢʜᴛᴍᴏᴅᴇ ʀᴇᴍᴏᴠᴇᴅ ғʀᴏᴍ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ !**")
+            elif not check_night:
+                await query.message.edit_caption("**๏  ɴɪɢʜᴛᴍᴏᴅᴇ ɪs ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ  ɪɴ ᴛʜɪs ᴄʜᴀᴛ.**") 
+            
+    
+    
+async def start_nightmode() :
+    chats = []
+    schats = await get_nightchats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    if len(chats) == 0:
         return
-    if is_nightmode_indb(str(event.chat_id)):
-        await event.reply("This Chat is Has Already Enabled Night Mode.")
-        return
-    add_nightmode(str(event.chat_id))
-    await event.reply(
-        f"Added Chat {event.chat.title} With Id {event.chat_id} To Database. **This Group Will Be Closed On 12Am(IST) And Will Opened On 06Am(IST)**"
-    )
-
-
-@register(pattern="^/rmnight")
-async def disable_ws(event):
-    if event.is_group:
-        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-            await event.reply("🤦🏻‍♂️You are not admin so you can't use this command...")
-            return
-
-    if not event.is_group:
-        await event.reply("You Can Only Disable Night Mode in Groups.")
-        return
-    if not is_nightmode_indb(str(event.chat_id)):
-        await event.reply("This Chat is Has Not Enabled Night Mode.")
-        return
-    rmnightmode(str(event.chat_id))
-    await event.reply(
-        f"Removed Chat {event.chat.title} With Id {event.chat_id} From Database."
-    )
-
-
-async def job_close():
-    ws_chats = get_all_chat_id()
-    if len(ws_chats) == 0:
-        return
-    for warner in ws_chats:
+    for add_chat in chats:
         try:
-            await tbot.send_message(
-                int(warner.chat_id),
-                f"12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By {BOT_NAME}**",
-            )
-            await tbot(
-                functions.messages.EditChatDefaultBannedRightsRequest(
-                    peer=int(warner.chat_id), banned_rights=hehes
-                )
-            )
+            await app.send_photo(
+                add_chat,
+                photo="https://telegra.ph//file/06649d4d0bbf4285238ee.jpg",
+                caption= f"**ᴍᴀʏ ᴛʜᴇ ᴀɴɢᴇʟs ғʀᴏᴍ ʜᴇᴀᴠᴇɴ ʙʀɪɴɢ ᴛʜᴇ sᴡᴇᴇᴛᴇsᴛ ᴏғ ᴀʟʟ ᴅʀᴇᴀᴍs ғᴏʀ ʏᴏᴜ. ᴍᴀʏ ʏᴏᴜ ʜᴀᴠᴇ ʟᴏɴɢ ᴀɴᴅ ʙʟɪssғᴜʟ sʟᴇᴇᴘ ғᴜʟʟ ᴏғ ʜᴀᴘᴘʏ ᴅʀᴇᴀᴍs.\n\nɢʀᴏᴜᴘ ɪs ᴄʟᴏsɪɴɢ ɢᴏᴏᴅ ɴɪɢʜᴛ ᴇᴠᴇʀʏᴏɴᴇ  !**")
+            
+            await app.set_chat_permissions(add_chat,CLOSE_CHAT)
+
         except Exception as e:
-            logger.info(f"Unable To Close Group {warner} - {e}")
+            LOG.print(f"[bold red] Unable To close Group {add_chat} - {e}")
 
-
-# Run everyday at 12am
 scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
-scheduler.add_job(job_close, trigger="cron", hour=23, minute=59)
+scheduler.add_job(start_nightmode, trigger="cron", hour=23, minute=59)
+scheduler.start()
+
+async def close_nightmode():
+    chats = []
+    schats = await get_nightchats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    if len(chats) == 0:
+        return
+    for rm_chat in chats:
+        try:
+            await app.send_photo(
+                rm_chat,
+                photo="https://telegra.ph//file/14ec9c3ff42b59867040a.jpg",
+                caption= f"**ɢʀᴏᴜᴘ ɪs ᴏᴘᴇɴɪɴɢ ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ ᴇᴠᴇʀʏᴏɴᴇ !\n\nᴍᴀʏ ᴛʜɪs ᴅᴀʏ ᴄᴏᴍᴇ ᴡɪᴛʜ ᴀʟʟ ᴛʜᴇ ʟᴏᴠᴇ ʏᴏᴜʀ ʜᴇᴀʀᴛ ᴄᴀɴ ʜᴏʟᴅ ᴀɴᴅ ʙʀɪɴɢ ʏᴏᴜ ᴇᴠᴇʀʏ sᴜᴄᴄᴇss ʏᴏᴜ ᴅᴇsɪʀᴇ. Mᴀʏ ᴇᴀᴄʜ ᴏғ ʏᴏᴜʀ ғᴏᴏᴛsᴛᴇᴘs ʙʀɪɴɢ Jᴏʏ ᴛᴏ ᴛʜᴇ ᴇᴀʀᴛʜ ᴀɴᴅ ʏᴏᴜʀsᴇʟғ. ɪ ᴡɪsʜ ʏᴏᴜ ᴀ ᴍᴀɢɪᴄᴀʟ ᴅᴀʏ ᴀɴᴅ ᴀ ᴡᴏɴᴅᴇʀғᴜʟ ʟɪғᴇ ᴀʜᴇᴀᴅ.**")
+            
+            await app.set_chat_permissions(rm_chat,OPEN_CHAT)
+
+        except Exception as e:
+            LOG.print(f"[bold red] Unable To open Group {rm_chat} - {e}")
+
+scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
+scheduler.add_job(close_nightmode, trigger="cron", hour=6, minute=1)
 scheduler.start()
 
 
-async def job_open():
-    ws_chats = get_all_chat_id()
-    if len(ws_chats) == 0:
-        return
-    for warner in ws_chats:
-        try:
-            await tbot.send_message(
-                int(warner.chat_id),
-                f"06:00 Am, Group Is Opening.\n**Powered By {BOT_NAME}**",
-            )
-            await tbot(
-                functions.messages.EditChatDefaultBannedRightsRequest(
-                    peer=int(warner.chat_id), banned_rights=openhehe
-                )
-            )
-        except Exception as e:
-            logger.info(f"Unable To Open Group {warner.chat_id} - {e}")
 
-
-# Run everyday at 06
-scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
-scheduler.add_job(job_open, trigger="cron", hour=6, minute=1)
-scheduler.start()
-
-__help__ = """
-*ᴀᴅᴍɪɴs ᴏɴʟʏ*
-
- ❍ /nightmode *:* ᴀᴅᴅs ɢʀᴏᴜᴘ ᴛᴏ ɴɪɢʜᴛᴍᴏᴅᴇ ᴄʜᴀᴛs
- ❍ /rmnight *:* ʀᴇᴍᴏᴠᴇs ɢʀᴏᴜᴘ ғʀᴏᴍ ɴɪɢʜᴛᴍᴏᴅᴇ ᴄʜᴀᴛs
-
-*ɴᴏᴛᴇ:* ɴɪɢʜᴛ ᴍᴏᴅᴇ ᴄʜᴀᴛs ɢᴇᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴄʟᴏsᴇᴅ ᴀᴛ 12 ᴀᴍ(ɪsᴛ) ᴀɴᴅ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ᴏᴘᴇɴɴᴇᴅ ᴀᴛ 6 ᴀᴍ(ɪsᴛ) ᴛᴏ ᴘʀᴇᴠᴇɴᴛ ɴɪɢʜᴛ sᴘᴀᴍs.
-
-☆............𝙱𝚈 » [𓆰ꪑ𝓲𝓽𝘴𝓾𝘳𝓲  𝘬ꪖ ꪜ𝓲ꪶꪶ𝓲ꪖꪀ𓆪](https://t.me/villen_012)............☆
-"""
-
-__mod_name__ = "⚡Nɪɢʜᴛ⚡"
